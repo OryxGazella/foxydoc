8e81cea783c0c8c92f4c5b777378afa297f880d4
Frank Smith <oryx.gazella@gmail.com>
2013-06-13 03:50:26 +0200

    Add the logo.

--- write_documentation.sh
+++ write_documentation.sh
@@ -2,7 +2,10 @@
 mkdir temp
 cp Readme.md temp
 cd temp
-pandoc -s Readme.md -o Readme.tex
+pandoc -s Readme.md -H ../custom_includes.tex -o Readme.tex
+sed -i -e 's/documentclass\[\]/documentclass\[titlepage\]/' "Readme.tex"
+sed -i '/\\date.*$/a\
+\\titlepic{\\includegraphics\[width=5cm\]{..\/frog\.pdf}}' "Readme.tex"
 # yes, three times for intra doc refrences and the like.
 pdflatex Readme.tex
 pdflatex Readme.tex
